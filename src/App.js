203b8fd544acc68a5050f30a1bd7c21af3fbd71b
Vaibhav Aswal <90501372+VaibhavAswal@users.noreply.github.com>
2022-11-08 00:38:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Home from "./Pages/Home/Home";
 function App() {
 		let vh = window.innerHeight;
 		document.documentElement.style.setProperty("--vh", `${vh}px`);
-	window.addEventListener("resize", function () {
-		let vh = window.innerHeight;
-		document.documentElement.style.setProperty("--vh", `${vh}px`);
-	});
+	
 	return (
 		<div className="App">
 			<NavBar />
