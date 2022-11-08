1eb1ee7d316ca1b7ad7e9050c1f524b98823814f
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-11-09 01:34:59 +0530

    changed app component from image to code

--- src/Components/Getapp/index.jsx
+++ src/Components/Getapp/index.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import getapp from "../../Images/getapp.png";
+import icon from "../../Images/gplay.png";
+import app from "../../Images/appss.png";
 import "./Getapp.css";
 const Getapp = () => {
 	return (
-		<div className="bg-[#001D25] getApp">
-			<img src={getapp} alt="getApp"></img>
+		<div className="getApp">
+			<div className="getAppContentB">
+				<div className="getAppContent">
+					<p className="title">Get App</p>
+					<p className="desc">
+						A fest is totally incomplete without the the event which appreciates
+						our techy side.
+					</p>
+					<p className="downloadText">Download App</p>
+					<img src={icon} alt="googlePlay" />
+				</div>
+				<img src={app} alt="app" className="getAppContentSS" />
+			</div>
 		</div>
 	);
 };
