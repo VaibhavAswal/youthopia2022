9e91e9026edebf6111f070119d0f1123a8b1c34f
shreyal0007 <shreyaljain0007@gmail.com>
2022-11-08 13:06:47 +0530

    getapp and youthopia component added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Landing from "../../Components/Landing/Landing";
 import Sponsors from "../../Components/Sponsors/Sponsors";
+import Youthopiacomp from "../../Components/Youthopiacomp";
+import Getapp from "../../Components/Getapp";
 
 const Home = () => {
 	return (
 		<>
 			<Landing />
 			<Sponsors />
+			<Getapp/>
+	        <Youthopiacomp/>
 		</>
 	);
 };
