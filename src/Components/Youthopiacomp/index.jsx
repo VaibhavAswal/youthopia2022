4fabe1c0c500987d5085c77ffac6b43f948d25df
Vaibhav Aswal <vbhv.aswal@gmail.com>
2022-11-09 00:44:49 +0530

    fixed responsiveness for YouthopiaComponent

--- src/Components/Youthopiacomp/index.jsx
+++ src/Components/Youthopiacomp/index.jsx
@@ -1,37 +1,45 @@
-        import React from "react";
-        import "./Youthopiacomp.css"
-        import imageS from "../../Images/imageS.png";
-        import DITU from "../../Images/DITU.png";
-        
-        import Line from "../../Images/Line.png";
-        import whitevariant from "../../Images/whitevariant.png";
-        const Youthopiacomp =() =>
-        {
-            return(
-<div className="main bg-[#001D25] ">
-        <div className="container1 flex flex-row">
-            <div className="left bg-[#001D25] ">
-                <img className="w-96 h-96" src={imageS} alt="image not available"></img>
-            </div>
-            <div className="right bg-[#001D25]">
-                <div className="class1">
-                    <div className="class2 flex flex-row space-x-3">
-                        <img className="whitevariant" src={whitevariant} alt="image not available"></img>
-                        <img  className="Line " src={Line} alt="image not available"></img>
-                        <img className="DITU" src={DITU} alt="image not available"></img>
-                    </div>
-                    <div>
-                        <p className="text1">Surrender your typical, boring college life. Mark the dates because Youthopia'22 is making a comeback at DIT University. An annual techno-cultural event binds tech geeks and cultural artists in circumstances where talents will be uncovered, revealing entirely different euphoric.</p>
-                    </div>
-                    <div>
-                        <br/> <p  className="text2">Read More.</p>
-                    </div>
-                </div>
+import React from "react";
+import imageS from "../../Images/imageS.png";
+import DITU from "../../Images/DITU.png";
+import arrow from "../../Images/yArrow-right.png";
+import "./Youthopiacomp.css";
 
-            </div>
-        </div>
-    </div>
-            )
-        }
+import Line from "../../Images/Line.png";
+import whitevariant from "../../Images/whitevariant.png";
+const Youthopiacomp = () => {
+	return (
+		<div className="youthopiacomp">
+			<div className="youthopiaContent">
+				<div className="youthopiacompLeft">
+					<img src={imageS} alt="youthopia"></img>
+				</div>
+				<div className="youthopiaRight">
+					<div className="yRightTop">
+						<img
+							className="ywhitevariant"
+							src={whitevariant}
+							alt="youthopia"
+						></img>
+						<img className="yLine " src={Line} alt="youthopia"></img>
+						<img className="yDITU" src={DITU} alt="youthopia"></img>
+					</div>
+					<div className="yRightMiddle">
+						<p className="text1">
+							Surrender your typical, boring college life. Mark the dates
+							because Youthopia'22 is making a comeback at DIT University. An
+							annual techno-cultural event binds tech geeks and cultural artists
+							in circumstances where talents will be uncovered, revealing
+							entirely different euphoric.
+						</p>
+					</div>
+					<div className="yRightEnd">
+						<p className="text2">Read More</p>
+						<img src={arrow} alt="arrow" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-        export default Youthopiacomp;
+export default Youthopiacomp;
