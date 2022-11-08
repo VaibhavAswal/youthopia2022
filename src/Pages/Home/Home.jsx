8befa15ee214ca9ab616325ec4ed35811a5e6e2b
Tanishq <tanishqjain1652003@gmail.com>
2022-11-08 13:15:40 +0530

    added star night component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Landing from "../../Components/Landing/Landing";
 import Sponsors from "../../Components/Sponsors/Sponsors";
+import StarNight from "../../Components/StarNight/StarNight";
 
 const Home = () => {
 	return (
 		<>
 			<Landing />
 			<Sponsors />
+			<StarNight/>
 		</>
 	);
 };
